934f2c5681d1599c9ad583a273563a3b2c6f0a4a
Ramon Pessoa <ramon.palheta.pessoa@hotmail.com>
2020-06-09 15:13:39 -0400

    Alteração final na aula1

--- Aula1.php
+++ Aula1.php
@@ -3,3 +3,23 @@
 // iniciando a jornada
 
 // PHP com BD
+
+echo "<font color =#ff0000>Primeira linha em PHP</font><br/>";
+echo "Curso de PHP";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 1 de PHP</title>
+</head>
+<body>
+    <?php
+        echo "Jornnada PHP";
+
+    ?>
+</body>
+</html>
