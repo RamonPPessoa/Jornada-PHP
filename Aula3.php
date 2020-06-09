5dc337f8ee447a2183bad53fd1e619946f6b4ed0
Ramon Pessoa <ramon.palheta.pessoa@hotmail.com>
2020-06-09 16:29:08 -0400

    Aula3

---
+++ Aula3.php
@@ -0,0 +1,19 @@
+<?php
+
+    // OPERAÇÕES COM VARIÁVEIS
+
+
+
+    $num1 = 10;
+    $num2 = 5;
+
+    $res = $num1 + $num2;
+    echo " A soma de " .$num1. "+" .$num2. "=" .$res. "<br/>";
+
+    // $num1 = $num1 + 1;
+    $num1++;
+    echo "Novo valor de num1: $num1<br/>";
+
+    $num2+=2;
+
+    echo "Novo valor de num2: $num2<br/>";
