b0c47e6f81b7a679e9fb61f9af68e1fbf458f9f1
Ramon Pessoa <ramon.palheta.pessoa@hotmail.com>
2020-06-13 18:53:31 -0400

    Aula 6

---
+++ Aula6.php
@@ -0,0 +1,43 @@
+<?php
+ $opc = 1;
+
+ switch($opc)
+ {
+     case 1:
+        echo "Carro";
+        break;
+    case 2:
+        echo "Moto";
+        break;
+    case 3:
+        echo "Bicicleta";
+        break;
+    case 4:
+        echo "Navio";
+        break;
+    case 5:
+        echo "Avião";
+        break;
+    default:
+        echo "Opão invalida";
+        break;
+
+
+
+ }
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 6 de PHP - Condicional Swutch case</title>
+</head>
+<body>
+    
+</body>
+</html>
