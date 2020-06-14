32766b6ee230d27123a981f45c71173e86f74690
Ramon Pessoa <ramon.palheta.pessoa@hotmail.com>
2020-06-14 08:04:17 -0400

    Aula8

---
+++ Aula8.php
@@ -0,0 +1,26 @@
+<?php
+$i = 8;
+$tam = 5;
+
+do
+{
+    echo "<pre>Variavel i tem o valor: $i<br/>";
+    echo "variavel tam tem o valor: $tam</pre><br/>";
+    $i++;
+}
+
+while($i < $tam);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loop DO While</title>
+</head>
+<body>
+    
+</body>
+</html>
