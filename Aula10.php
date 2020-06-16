43244aa11c1f933587de90b90b82dcdcc58270a8
Ramon Pessoa <ramon.palheta.pessoa@hotmail.com>
2020-06-16 07:32:00 -0400

    Aula 10

---
+++ Aula10.php
@@ -0,0 +1,27 @@
+<?php
+
+$transp = array("IRON MAIDEN", "DEEP PURPLE","THE WHO","MOTORHEAD","BLACK SABBATH","TITÃS");
+
+foreach($transp as $veiculo)
+{
+    echo $veiculo. "<br/>";
+    /*
+    if($veiculo == "THE WHO")
+        break;
+        */
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loop FOREACH</title>
+</head>
+<body>
+    
+</body>
+</html>
