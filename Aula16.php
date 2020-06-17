82ede69c63cfe2476ac0dcdfccde97e81164b391
Ramon Pessoa <ramon.palheta.pessoa@hotmail.com>
2020-06-17 16:06:24 -0400

    Aula 16 - Include

---
+++ Aula16.php
@@ -0,0 +1,21 @@
+<?php
+
+    include "data.php";
+
+    echo "</br>";
+    include "menu.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 16 - Include</title>
+</head>
+<body>
+    
+</body>
+</html>
