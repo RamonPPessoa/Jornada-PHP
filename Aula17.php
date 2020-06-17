4569c1765ede0fa3636a7c5586c0c17a65bc5201
Ramon Pessoa <ramon.palheta.pessoa@hotmail.com>
2020-06-17 17:00:06 -0400

    Aula17 -Isset 3

--- Aula17.php
+++ Aula17.php
@@ -2,12 +2,14 @@
 
 
 
-if(isset($_POST["f_nome"]))
+if(isset($_POST["f_nome"]) && ($_POST["f_estado"]))
 
 
 {
     $vnome = $_POST["f_nome"];
+    $vestado = $_POST["f_estado"];
     echo "Nome: $vnome<br/>"; 
+    echo "Nome: $vestado<br/>"; 
 
 }
 else
@@ -30,6 +32,8 @@ else
     <form name="f_aula" action="Aula17.php" method="post">
     <label for="">Nome:</label></br>
     <input type="text" name="f_nome"><br>
+    <label for="">Estado:</label></br>
+    <input type="text" name="f_estado"><br>
     <input type="submit" value="Enviar">
     
     
